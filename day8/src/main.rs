948d0dcb6bed483bc6658faa0cf1a533834cd236
Jay Vana <jaysvana@gmail.com>
2019-12-07 21:41:05 -0800

    Day 8 part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -36,40 +36,37 @@ fn main() -> Result<()> {
     let width = 25;
     let height = 6;
 
-    let mut min_count: usize = std::usize::MAX;
-    let mut min_layer = None;
+    let mut result = String::with_capacity(width * height);
+
     let layer_count = image.len() / (width * height);
 
-    let zero = '0';
+    for y in 0..height {
+        for x in 0..width {
+            let mut found = false;
+            for layer in 0..layer_count {
+                let layer_size = width * height;
+                let layer_index = layer_size * layer + y * width + x;
+                let color = &image[layer_index..layer_index + 1];
 
-    for layer_index in 0..layer_count {
-        let layer = get_layer(&image, width, height, layer_index);
-        let digit_count = count_digits(layer);
+                if color == "0" {
+                    result.push_str(" ");
+                    found = true;
+                    break;
+                }
 
-        let zero_count = *digit_count.get(&zero).unwrap_or(&0);
-        if zero_count < min_count {
-            min_count = zero_count;
-            min_layer = Some(layer_index);
+                if color == "1" {
+                    result.push_str(color);
+                    found = true;
+                    break;
+                }
+            }
+            if !found {
+                result.push_str(" ");
+            }
         }
     }
 
-    println!("Min layer: {}", min_layer.unwrap());
-    print_layer(
-        get_layer(&image, width, height, min_layer.unwrap()),
-        width,
-        height,
-    );
-
-    match min_layer {
-        Some(layer_index) => {
-            let digit_count = count_digits(get_layer(&image, width, height, layer_index));
-
-            let one_count = *digit_count.get(&'1').unwrap_or(&0);
-            let two_count = *digit_count.get(&'2').unwrap_or(&0);
-            println!("Total multiplied: {}", one_count * two_count);
-        }
-        None => println!("WAT"),
-    }
+    print_layer(&result, width, height);
 
     Ok(())
 }
